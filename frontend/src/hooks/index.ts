3c0ed089ba20a72a89aa877b06c8133560ee7da9
develper21 <githubnarvin@gmail.com>
2026-01-02 08:46:43 +0530

    🗑️ Remove old frontend directory structure

--- frontend/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all hooks from a central index
-export {useMeterReadings} from './useMeterReadings';
-export {useLocation} from './useLocation';
-export {useOCREngine} from './useOCREngine';
-export {useOfflineMode} from './useOfflineMode';
-export {useCameraQuality} from './useCameraQuality';
