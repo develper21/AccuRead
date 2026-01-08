1aba97dbf4384fd9a20313b2536ffe0373c79ede
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-08 21:35:59 +0530

    in this file i was resolve a some syntax error and reinstall a new packages and library

--- AccuRead/components/external-link.tsx
+++ AccuRead/components/external-link.tsx
@@ -1,8 +1,12 @@
-import { Href, Link } from 'expo-router';
+import Link from 'expo-router/link';
 import { openBrowserAsync, WebBrowserPresentationStyle } from 'expo-web-browser';
 import { type ComponentProps } from 'react';
+import { GestureResponderEvent } from 'react-native';
 
-type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: Href & string };
+
+type Props = Omit<ComponentProps<typeof Link>, 'href'> & {
+  href: string;
+};
 
 export function ExternalLink({ href, ...rest }: Props) {
   return (
@@ -10,7 +14,7 @@ export function ExternalLink({ href, ...rest }: Props) {
       target="_blank"
       {...rest}
       href={href}
-      onPress={async (event) => {
+      onPress={async (event: GestureResponderEvent) => {
         if (process.env.EXPO_OS !== 'web') {
           // Prevent the default behavior of linking to the default browser on native.
           event.preventDefault();
