fc0d32ff0b1097eb09687cbc6cdead19623e22c6
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-08 16:48:05 +0530

    feat: in this i can install a react-native package

--- AccuRead/app/(tabs)/index.tsx
+++ AccuRead/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import HomeScreenComponent from '../../screens/HomeScreen';
 
 export default function HomeScreen() {
