704a2741c1dcb72ce09278cfcd3c094ba3ca7e47
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-09 21:05:43 +0530

    Add hooks index file with centralized exports and hook documentation

--- AccuRead/hooks/index.ts
+++ AccuRead/hooks/index.ts
@@ -1,6 +1,18 @@
+/**
+ * Copyright (c) 2025 develper21
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
+ * IMPORTANT: Removal of this header violates the license terms.
+ * This code remains the property of develper21 and is protected
+ * under intellectual property laws.
+ */
+
 // Export all hooks from a central index
 export {useMeterReadings} from './useMeterReadings';
 export {useLocation} from './useLocation';
 export {useOCREngine} from './useOCREngine';
 export {useOfflineMode} from './useOfflineMode';
 export {useCameraQuality} from './useCameraQuality';
+export {useWorklist} from './useWorklist';
