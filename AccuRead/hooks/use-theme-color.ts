c2252f2e928c16ae40aa6a477baf1755fd79611f
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-09 21:12:04 +0530

    Add useThemeColor hook with dynamic color selection and theme-aware styling

--- AccuRead/hooks/use-theme-color.ts
+++ AccuRead/hooks/use-theme-color.ts
@@ -1,4 +1,15 @@
 /**
+ * Copyright (c) 2025 develper21
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
+ * IMPORTANT: Removal of this header violates the license terms.
+ * This code remains the property of develper21 and is protected
+ * under intellectual property laws.
+ */
+
+/**
  * Learn more about light and dark modes:
  * https://docs.expo.dev/guides/color-schemes/
  */
