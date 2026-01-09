dbf80265ce93091d53de0805dadb39c0ecf8a283
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-09 20:57:51 +0530

    Add ProcessingScreen component with loading states and OCR progress feedback

--- AccuRead/components/ProcessingScreen.tsx
+++ AccuRead/components/ProcessingScreen.tsx
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2025 develper21
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
+ * IMPORTANT: Removal of this header violates the license terms.
+ * This code remains the property of develper21 and is protected
+ * under intellectual property laws.
+ */
+
 import React from 'react';
 import {View, StyleSheet, Text, ActivityIndicator} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
