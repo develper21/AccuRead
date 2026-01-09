018390080f4fffffae40b3516c1fa834b2c18663
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-09 22:50:43 +0530

    Add config package initialization with settings imports and configuration management

---
+++ backend/config/__init__.py
@@ -0,0 +1,9 @@
+# Config package
+# Copyright (c) 2025 develper21
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+# 
+# IMPORTANT: Removal of this header violates the license terms.
+# This code remains the property of develper21 and is protected
+# under intellectual property laws.
