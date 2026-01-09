9c58f49d717c37b5cc31820ca39011ca0cd213c2
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-09 21:57:16 +0530

    Update image compression service with file size optimization, quality control, and format conversion

--- AccuRead/services/imageCompression.ts
+++ AccuRead/services/imageCompression.ts
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2025 develper21
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
+ * IMPORTANT: Removal of this header violates the license terms.
+ * This code remains the property of develper21 and is protected
+ * under intellectual property laws.
+ */
+
 import ImageResizer from 'react-native-image-resizer';
 import { Platform, Image } from 'react-native';
 import RNFS from 'react-native-fs';
