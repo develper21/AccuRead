7a336ed64b9a16c848d488b68dfd5fce63b07ae5
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-09 14:51:00 +0530

    Add authentication layout component with navigation structure

---
+++ AccuRead/app/(auth)/_layout.tsx
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2025 develper21
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
+ * IMPORTANT: Removal of this header violates the license terms.
+ * This code remains the property of develper21 and is protected
+ * under intellectual property laws.
+ */
+
+import { Stack } from 'expo-router';
+
+export default function AuthLayout() {
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="login" />
+        </Stack>
+    );
+}
